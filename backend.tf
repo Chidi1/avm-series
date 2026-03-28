df8f6530c3884a060c05419bcd31194ab80aa22b
Chidi1 <ugwuchidieberej@gmail.com>
2026-03-28 18:05:59 +0000

    tf ver

--- backend.tf
+++ backend.tf
@@ -1,4 +1,4 @@
-terraform {
+/*terraform {
   backend "azurerm" {
     resource_group_name  = "rg-avm-demo-001"
     storage_account_name = "avmtfstate"
@@ -7,4 +7,4 @@ terraform {
     subscription_id      = "3f08161e-7132-4a28-85ba-a39a7365e2d7" 
     use_azuread_auth     = true
   }
-}
+}*/
