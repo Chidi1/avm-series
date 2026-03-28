6850ec47f3687a68e3f0801756bdfc5dbf405387
Chidi1 <ugwuchidieberej@gmail.com>
2026-03-28 18:00:53 +0000

    tf ver

--- backend.tf
+++ backend.tf
@@ -4,6 +4,7 @@ terraform {
     storage_account_name = "avmtfstate"
     container_name       = "tfstate"
     key                  = "avm.tfstate"
+    subscription_id      = "3f08161e-7132-4a28-85ba-a39a7365e2d7" 
     use_azuread_auth     = true
   }
 }
