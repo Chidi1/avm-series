930547f1254efa8deb78fd5d7de86285624ad5ad
Chidi1 <ugwuchidieberej@gmail.com>
2026-03-28 17:56:18 +0000

    tf ver

--- main.tf
+++ main.tf
@@ -61,43 +61,10 @@ module "vm" {
   size           = "Standard_B2s"
 }
 
-############################################
-# 4. SQL + AUDITING
-############################################
-module "sql_server" {
-  source  = "Azure/avm-res-sql-server/azurerm"
 
-  name                = "${var.prefix}-sql"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  administrator_login          = var.sql_admin
-  administrator_login_password = var.sql_password
-  
-  # Add version tag for stability
-  version = "~> 0.1"
-}
-
-module "sql_db" {
-  source = "Azure/avm-res-sql-database/azurerm"
-
-  name      = "${var.prefix}-db"
-  server_id = module.sql_server.id
-  
-  version = "~> 0.1"
-}
-
-resource "azurerm_mssql_server_extended_auditing_policy" "audit" {
-  server_id                              = module.sql_server.id
-  enabled                                = true
-  storage_endpoint                       = module.storage_account.primary_blob_endpoint
-  storage_account_access_key             = module.storage_account.primary_access_key
-  
-  depends_on = [module.sql_server]
-}
 
 ############################################
-# 5. APP SERVICE
+# 3. APP SERVICE
 ############################################
 module "appservice" {
   source = "Azure/avm-res-web-site/azurerm"
@@ -112,7 +79,7 @@ module "appservice" {
 }
 
 ############################################
-# 6. REDIS
+# 4. REDIS
 ############################################
 module "redis" {
   source = "Azure/avm-res-cache-redis/azurerm"
@@ -129,7 +96,7 @@ module "redis" {
 }
 
 ############################################
-# 7. PRIVATE AKS + ACR
+# 5. PRIVATE AKS + ACR
 ############################################
 module "acr" {
   source = "Azure/avm-res-containerregistry-registry/azurerm"
@@ -173,142 +140,12 @@ module "aks_private" {
   depends_on = [module.acr]
 }
 
-############################################
-# 8. HUB & SPOKE
-############################################
-module "hubspoke" {
-  source = "Azure/avm-ptn-network-hubspoke/azurerm"
 
-  hub_vnet_cidr   = "10.10.0.0/16"
-  spoke_vnet_cidr = "10.20.0.0/16"
-  
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  
-  version = "~> 0.1"
-}
+
+
 
 ############################################
-# 9. POLICY + RBAC
-############################################
-module "policy" {
-  source = "Azure/avm-res-authorization-policyassignment/azurerm"
-
-  policy_definition_id = var.policy_definition_id
-  scope                = var.subscription_id
-  name                 = "${var.prefix}-policy-assignment"
-  
-  version = "~> 0.1"
-}
-
-############################################
-# 10. CI/CD AGENTS
-############################################
-module "agents" {
-  source = "Azure/avm-res-compute-virtualmachinescaleset/azurerm"
-
-  name                = "${var.prefix}-agents"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  instances           = 2
-  
-  sku = {
-    name     = "Standard_D2s_v3"
-    tier     = "Standard"
-    capacity = 2
-  }
-  
-  source_image_reference = {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-  
-  admin_username = "azureuser"
-  
-  admin_ssh_key = [{
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }]
-  
-  version = "~> 0.1"
-}
-
-############################################
-# 11. MONITORING
-############################################
-module "monitor" {
-  source = "Azure/avm-res-insights-diagnosticsetting/azurerm"
-
-  target_resource_id         = module.vm.id
-  name                       = "${var.prefix}-diag-setting"
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
-  
-  enabled_log = [
-    {
-      category = "Audit"
-    }
-  ]
-  
-  metric = [
-    {
-      category = "AllMetrics"
-    }
-  ]
-  
-  version = "~> 0.1"
-}
-
-############################################
-# 12. FUNCTION + PRIVATE ENDPOINT
-############################################
-module "function" {
-  source = "Azure/avm-res-web-functionapp/azurerm"
-
-  name                = "${var.prefix}-func"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  
-  runtime_stack = "dotnet"
-  runtime_version = "6.0"
-  
-  version = "~> 0.1"
-}
-
-resource "azurerm_private_endpoint" "func_pe" {
-  name                = "${var.prefix}-func-pe"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = module.vnet.subnets["app"].id
-  
-  private_service_connection {
-    name                           = "${var.prefix}-func-psc"
-    private_connection_resource_id = module.function.id
-    is_manual_connection           = false
-    subresource_names              = ["sites"]
-  }
-  
-  depends_on = [module.function]
-}
-
-############################################
-# 13. LANDING ZONE
-############################################
-module "landingzone" {
-  source = "Azure/avm-ptn-landingzone/azurerm"
-
-  environment = var.environment
-  location    = var.location
-  
-  # Add common landing zone configuration
-  subscription_id = var.subscription_id
-  
-  version = "~> 0.1"
-}
-
-############################################
-# 14. AVD
+# 6. AVD
 ############################################
 module "avd" {
   source = "Azure/avm-res-desktopvirtualization-hostpool/azurerm"
@@ -324,7 +161,7 @@ module "avd" {
 }
 
 ############################################
-# 15. PRIVATE DNS
+# 7. PRIVATE DNS
 ############################################
 module "privatedns" {
   source = "Azure/avm-res-network-privatednszone/azurerm"
